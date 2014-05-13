070a72d79953483be07619d34464242c64bef119
Alan <acao@moneycloud.me>
2014-05-13 08:49:58 -0400

    still try to find the solution

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,29 +33,41 @@ angular
     $rootScope.direction = 'left'   //animation direction
 
     $rootScope.$on("$routeChangeStart", function(event, next, current){
-      //console.log('route change start triggered');
+      console.log('main route change start triggered');
       //console.log(current.scope);
       //console.log(current.$$route.originalPath);
       
-      $rootScope.from = current.$$route.originalPath;
-      $rootScope.to = next.$$route.originalPath;
-      console.log('to: '+$rootScope.to );
-      console.log('from: '+$rootScope.from );
-      var lastState = $rootScope.historyLog.pop()
-      if ($rootScope.to == lastState){
+      // $rootScope.from = current.$$route.originalPath;
+      // $rootScope.to = next.$$route.originalPath;
+      // console.log('to: '+$rootScope.to );
+      // console.log('from: '+$rootScope.from );
+
+      $rootScope.back = function(){
+        console.log('back triggerred');
         $rootScope.direction = 'right';
-        if(!current.scope.$$phase) {
-          current.scope.$apply();
-        }
-        
-      }else{
-        $rootScope.historyLog.push(lastState);  //push back the previous poped state
-        $rootScope.historyLog.push($rootScope.from);
-        $rootScope.direction = 'left';
-        if(!current.scope.$$phase) {
-          current.scope.$apply();
-        }
+        // $location.path('/about');
       }
+
+      $rootScope.forward = function(){
+        console.log('forward triggerred');
+        $rootScope.direction = 'left'
+        // $location.path('/');
+      }
+      // var lastState = $rootScope.historyLog.pop()
+      // if ($rootScope.to == lastState){
+      //   $rootScope.direction = 'right';
+      //   if(!$rootScope.$$phase) {
+      //     $rootScope.$apply();
+      //   }
+        
+      // }else{
+      //   $rootScope.historyLog.push(lastState);  //push back the previous poped state
+      //   $rootScope.historyLog.push($rootScope.from);
+      //   $rootScope.direction = 'left';
+      //   if(!$rootScope.$$phase) {
+      //     $rootScope.$apply();
+      //   }
+      // }
       console.log($rootScope.historyLog);
     })
 
--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -1,17 +1,27 @@
 'use strict';
 
 angular.module('angularTestApp')
-  .controller('contactCtrl', function ($scope) {
+  .controller('contactCtrl', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
     console.log('contact init');
     // $scope.direction = $scope.$root.direction;
     // console.log('contact control' + $scope.direction);
+
+    // $scope.back = function(){
+    //     $rootScope.direction = 'right';
+    //     // $location.path('/about');
+    // }
+
+    // $scope.forward = function(){
+    //     $rootScope.direction = 'left'
+    //     // $location.path('/');
+    // }
 
     $scope.$on('$destroy', function(){
         console.log('contact ctrl destroy');
     })
 
     $scope.$on('$routeChangeStart', function(){
-        console.log('contact controller listener');
+        console.log('contact controller route change listener');
     })
 
-  });
+  }]);
