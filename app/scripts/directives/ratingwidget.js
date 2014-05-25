c9f7b3b72866bb2a8cf6d480e76e9f9c2038a146
Alan <acao@moneycloud.me>
2014-05-25 19:10:42 -0400

    controller vs link function in directive

--- app/scripts/directives/ratingwidget.js
+++ app/scripts/directives/ratingwidget.js
@@ -4,27 +4,26 @@ angular.module('angularTestApp').directive('ratingWidget', [function () {
     return {
         restrict: 'A,E',
         scope: {
-            stars: '='
+            //do not bind to parent scope
+            // stars: '='      
         },
         templateUrl: './views/directives/rating.html',
-        link: function (scope, iElement, iAttrs) {
-            // iElement.each(function(index){
-            //     console.log(index);
-            // })
-            $('li').each(function(){
-                // $(this).on('click', function(){
-                //     console.log('onclick');
-                //     scope.stars.pop();
-                // })
-                console.log(this);
-            });
-
+        // link: function (scope, iElement, iAttrs) {
+        //     scope.stars = []
+        //     $('ul').on('click', function(){
+        //         console.log('click');
+        //         scope.stars.push('');
+        //         scope.$apply();
+        //     })  
+        // }
+        controller: ['$scope',function($scope){
+            console.log($scope);
+            $scope.stars = [];
             $('ul').on('click', function(){
                 console.log('click');
-                scope.stars.pop();
-                scope.$apply();
-            })
-            
-        }
+                $scope.stars.push('');
+                $scope.$apply();
+            }) 
+        }]
     };
 }])
