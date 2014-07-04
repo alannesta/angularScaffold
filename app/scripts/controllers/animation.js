41a3870822db37c621deb877d16e111d6911be31
Alan <acao@moneycloud.me>
2014-07-03 23:08:16 -0400

    tweak

--- app/scripts/controllers/animation.js
+++ app/scripts/controllers/animation.js
@@ -6,16 +6,24 @@ angular.module('angularTestApp')
     $scope.animate = function(){
         var div = $('#tweenmaxblock');
         var div2 = $('#jqueryblock');
+
+
         div2.animate({
             width:200,
             top:100,
             left:100
         },1500, function(){
-            TweenLite.to(div, 1.5, {width:200, top:100,left:100});
-        })
+            div2.animate({
+                width:100,
+                top:0,
+                left:0
+            },1500)
+        });
 
-        
-        
+        // TweenLite.to(div, 1.5, {width:200, top:100,left:100});
+        TweenLite.to(div, 1.5, {width:200, x:100, y:100});
+        TweenLite.to(div, 1.5, {width:100, x:0, y:0, delay:1.5});
+
         
     }
 
