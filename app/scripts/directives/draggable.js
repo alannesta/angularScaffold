afa01281dcd36b3943f6de88221bdc75ba1b8624
sijin cao <scao@acquisio.com>
2015-04-14 13:23:29 -0400

    preparation for transfer object

--- app/scripts/directives/draggable.js
+++ app/scripts/directives/draggable.js
@@ -1,19 +1,21 @@
-angular.module('angularTestApp').directive('draggable', function(){
-    return {
-    	restrict: 'A',
-        // scope: {
-            
-        // },
-        link: function(scope, element, attrs){
-            el = element[0];
-        	el.ondragstart = function(event){
-                event.dataTransfer.effectAllowed = "move";
-                event.dataTransfer.setData("cardName", $(this).attr('cardName'));
-            }
-            el.ondragend = function(event){
-                //cannot get mouse position properly here, rather set the position on drop zone;
-                // $(this).removeClass('dragging');
-            }
-        }
+angular.module('angularTestApp').directive('draggable', function() {
+  return {
+    restrict: 'A',
+    // scope: {
+
+    // },
+    link: function(scope, element, attrs) {
+      el = element[0];
+      el.ondragstart = function(event) {
+        event.dataTransfer.effectAllowed = "move";
+
+        // TODO: scope.eval(card), setData: transfer the card object
+        console.log(attr);
+        event.dataTransfer.setData("cardName", $(this).attr('cardName'));
+      }
+      el.ondragend = function(event) {
+        console.log('dragend');
+      }
     }
+  }
 })
--- app/scripts/directives/droppable.js
+++ app/scripts/directives/droppable.js
@@ -1,32 +1,38 @@
-angular.module('angularTestApp').directive('droppable', function(){
-    return {
-    	restrict: 'A',
-        // scope: {
-            
-        // },
-        link: function(scope, element, attrs){
-            el = element[0];
-        	
-            el.addEventListener(
-                'dragenter',
-                function(e) {
-                    this.classList.add('over');
-                    return false;
-                },
-                false
-            );
+angular.module('angularTestApp').directive('droppable', function() {
+  return {
+    restrict: 'A',
+    scope: {
+      onDrop: '&',
+      itemList: '='
+    },
+    link: function(scope, element, attrs) {
+      el = element[0];
 
-            el.addEventListener(
-                'dragleave',
-                function(e) {
-                    this.classList.remove('over');
-                    return false;
-                },
-                false
-            );
+      el.addEventListener('dragenter', function() {
+        this.classList.add('over');
+        return false;
+      }, false);
 
-            
+      el.addEventListener('dragleave', function() {
+        this.classList.remove('over');
+        return false;
+      }, false);
 
+      el.on("drop", function(e) {
+        if (e.preventDefault) {
+          e.preventDefault(); // Necessary. Allows us to drop.
         }
+
+        if (e.stopPropogation) {
+          e.stopPropogation(); // Necessary. Allows us to drop.
+        }
+
+        var data = e.dataTransfer.getData("cardName");
+        console.log(data);
+        selectedCards
+        scope.onDrop({dragEl: src, dropEl: dest});
+      });
+
     }
+  }
 })
